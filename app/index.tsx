9181f5d2f41ec863e52f1a7aa7da5973feb5d4fb
rksdeveloper <raj7234271@gmail.com>
2026-03-20 11:28:48 +0530

    reset project folder

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import{ View, Text } from 'react-native';
+const App= ()=>{
+  return(
+    <View style={{height:640, width:406, backgroundColor:'black',borderRadius:70}}>
+      <Text style={{fontSize:20, color:'#fff',textAlign:'center', padding:10}}>Hello Tony Stark</Text>
+    </View>
+
+  );
+};
+export default App;
